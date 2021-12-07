3be11ecc7f1e317f6ca65cce6bcc3d4625918560
Fupin Yao <xcmax@qq.com>
2021-12-06 20:31:18 -0800

    d1

---
+++ src/Home.js
@@ -0,0 +1,40 @@
+import React from "react";
+import BarChart from "./BarChart";
+import { Canvas } from "@react-vertex/core";
+import { useViewportSize } from "@react-vertex/dom-hooks";
+import Scene from "./Scene";
+import { render } from "react-dom";
+import Tree from "./Tree";
+import data from "./data";
+
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+
+function Example() {
+    // const { width, height } = useViewportSize();
+  
+    return (
+      <Canvas width={400} height={400}>
+        <Scene />
+      </Canvas>
+    );
+  }
+const App = () => {
+    return <Tree data={data} width={400} height={400} />;
+};
+
+function Home() {
+    return (
+        <div>
+            <BarChart style={{width:400, height:400}}/>
+            <Example style={{width:400, height:400}}/>
+            <App style={{width:400, height:400}}/>
+        </div>
+    );
+}
+
+export default Home;
